49e01c0669c7e646db582c7de8c72a45edd48c4e
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-16 19:27:55 +0530

    modified

--- conditions.sh
+++ conditions.sh
@@ -3,9 +3,12 @@ if [ ID -ne 0 ]
 then
     echo"you have to be rooot user"
 else
-    echo"you are root user'
+    echo"you are root user"
+    
 fi
 
-yum install nginx -y
+sudo yum install nginx -y
+
 systemctl enable nginx
+
 systemctl start nginx
